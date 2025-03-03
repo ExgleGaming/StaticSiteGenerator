3b244dd6992fb020964f61ffa42202fb0bcb0c9b
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-03-02 20:44:52 -0500

    restarted project

--- src/extractmarkdownimages.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def extract_markdown_images(text):
-    # pattern = r"!\[(.*?)]\((.*?)\)"
-    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    return re.findall(pattern, text)
--- src/extractmarkdownlinks.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-def extract_markdown_links(text):
-    # pattern = r"\[(.*?)]\((.*?)\)"
-    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    return re.findall(pattern, text)
